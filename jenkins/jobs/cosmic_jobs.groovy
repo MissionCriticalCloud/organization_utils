cac27be04bd34c32ce8efa45f169a75ba3fbf0b6
Miguel Ferreira <miguelferreira@me.com>
2016-01-29 13:44:05 +0100

    Add multi job to build cosmic with maven step

--- jenkins/jobs/cosmic_jobs.groovy
+++ jenkins/jobs/cosmic_jobs.groovy
@@ -1,10 +1,9 @@
-def COSMIC_FOLDER_NAME = 'cosmic'
-
 def DEFAULT_GIT_REPO_BRANCH = 'remotes/origin/pr/*/head'
 
-def GENERIC_MAVEN_JOB      = "${COSMIC_FOLDER_NAME}/generic-maven-job"
-def UPDATE_COSMIC_REPO_JOB = "${COSMIC_FOLDER_NAME}/update-cosmic-repo"
-def SEED_JOB               = "${COSMIC_FOLDER_NAME}/seed-job"
+def DEFAULT_GIT_REPO_BRANCH_PARAM = 'sha1'
+def CUSTOM_WORKSPACE_PARAM        = 'CUSTOM_WORKSPACE'
+
+def WORKSPACE_VAR = '${WORKSPACE}'
 
 def DEFAULT_GITHUB_USER_NAME         = 'mccd-jenkins'
 def ORG_UTILS_GITHUB_REPOSITORY      = 'MissionCriticalCloud/organization_utils'
@@ -16,126 +15,199 @@ def GITHUB_OAUTH2_CREDENTIAL_PARAM = 'mccdJenkinsOauth2'
 
 def GITHUB_OAUTH2_TOKEN_ENV_VAR   = 'MCCD_JENKINS_OAUTH2_TOKEN'
 
-folder(COSMIC_FOLDER_NAME)
+def MCCD_JENKINS_GITHUB_CREDENTIALS       = 'f4ec9d6e-49fb-497c-bd1f-e42d88e105da'
+def MCCD_JENKINS_GITHUB_OAUTH_CREDENTIALS = '95c201f6-794e-434b-a667-cf079aac4dfc'
 
-freeStyleJob(SEED_JOB) {
-  logRotator {
-    numToKeep(10)
-    artifactNumToKeep(10)
-  }
-  label('executor')
-  scm {
-    git {
-      remote {
-        github(ORG_UTILS_GITHUB_REPOSITORY, 'https' )
+def FOLDERS = [
+  'cosmic',
+  'cosmic-dev'
+]
+
+def DEFAULT_EXECUTOR = 'executor-mct'
+
+FOLDERS.each { folderName ->
+  def seedJob             = "${folderName}/901-seed-job"
+  def updateCosmicRepoJob = "${folderName}/902-update-cosmic-repo"
+  def genericMavenJob     = "${folderName}/903-generic-maven-job"
+  def fullBuildJobName    = "${folderName}/001-full-build"
+
+
+  def isDevFolder = folderName.endsWith('-dev')
+  def executorLabel = DEFAULT_EXECUTOR + (isDevFolder ? '-dev' : '')
+
+  folder(folderName)
+
+  freeStyleJob(seedJob) {
+    logRotator {
+      numToKeep(10)
+      artifactNumToKeep(10)
+    }
+    label(executorLabel)
+    scm {
+      git {
+        remote {
+          github(ORG_UTILS_GITHUB_REPOSITORY, 'https' )
+        }
+        branch(DEFAULT_GITHUB_REPOSITORY_BRANCH)
+        shallowClone(true)
+        clean(true)
+        configure { node ->
+          node / 'extensions' << 'hudson.plugins.git.extensions.impl.PathRestriction' {
+            includedRegions 'jenkins/jobs/repo_jobs[.]groovy'
+            excludedRegions ''
+          }
+        }
       }
-      branch(DEFAULT_GITHUB_REPOSITORY_BRANCH)
-      shallowClone(true)
-      clean(true)
-      configure { node ->
-        node / 'extensions' << 'hudson.plugins.git.extensions.impl.PathRestriction' {
-          includedRegions 'jenkins/jobs/repo_jobs[.]groovy'
-          excludedRegions ''
+    }
+    triggers {
+      githubPush()
+    }
+    steps {
+      dsl {
+        external('jenkins/jobs/cosmic_jobs.groovy')
+      }
+    }
+  }
+
+  freeStyleJob(updateCosmicRepoJob) {
+    logRotator {
+      numToKeep(100)
+      artifactNumToKeep(10)
+    }
+    label(executorLabel)
+    wrappers {
+      colorizeOutput('xterm')
+      timestamps()
+    }
+    scm {
+      git {
+        remote {
+          github(COSMIC_GITHUB_REPOSITORY, 'ssh' )
+          credentials(MCCD_JENKINS_GITHUB_CREDENTIALS)
+        }
+        branch(DEFAULT_GITHUB_REPOSITORY_BRANCH)
+        shallowClone(false)
+        clean(true)
+        recursiveSubmodules(true)
+        trackingSubmodules(true)
+      }
+    }
+    triggers {
+      if (!isDevFolder) {
+        cron('H/15 * * * *')
+      }
+    }
+    steps {
+      shell([
+        'git config --global user.email "int-mccd_jenkins@schubergphilis.com"',
+        'git config --global user.name "mccd-jenkins"',
+        'if [ -z "$(git status -su)" ]; then',
+        '  echo "==> No submodule changed"',
+        'else',
+        '  echo "==> Updating all submodules in remote repository"',
+        '  git add --all',
+        '  git commit -m "Update all submodules to latest HEAD"',
+        '  git push origin HEAD:master',
+        'fi'
+      ].join('\n'))
+    }
+  }
+
+  mavenJob(genericMavenJob) {
+    parameters {
+      stringParam(GITHUB_REPOSITORY_NAME_PARAM, '', 'The GitHub repository to build')
+      stringParam(DEFAULT_GIT_REPO_BRANCH_PARAM, DEFAULT_GIT_REPO_BRANCH, 'Branch to be checked out and built')
+      credentialsParam(GITHUB_OAUTH2_CREDENTIAL_PARAM) {
+        type('org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl')
+        required()
+        defaultValue(MCCD_JENKINS_GITHUB_OAUTH_CREDENTIALS)
+        description('mccd jenkins OAuth2 token credential')
+      }
+      stringParam(CUSTOM_WORKSPACE_PARAM, WORKSPACE_VAR, 'A custom workspace to use for the job')
+    }
+    environmentVariables {
+      env(GITHUB_OAUTH2_CREDENTIAL_PARAM, "${GITHUB_REPOSITORY_NAME_PARAM}")
+    }
+    customWorkspace('${' + CUSTOM_WORKSPACE_PARAM + '}')
+    logRotator {
+      numToKeep(50)
+      artifactNumToKeep(50)
+    }
+    concurrentBuild()
+    label(executorLabel)
+    wrappers {
+      colorizeOutput('xterm')
+      timestamps()
+    }
+    scm {
+      git {
+        remote {
+          url('git@github.com:${' + GITHUB_REPOSITORY_NAME_PARAM + '}')
+          name('origin')
+          refspec('+refs/pull/*:refs/remotes/origin/pr/* +refs/heads/*:refs/remotes/origin/*')
+          credentials(MCCD_JENKINS_GITHUB_CREDENTIALS)
+        }
+        branch('${' + DEFAULT_GIT_REPO_BRANCH_PARAM + '}')
+        shallowClone(true)
+        clean(true)
+      }
+    }
+    goals('clean')
+    goals('install')
+    goals('-P developer')
+    publishers {
+      archiveJunit('**/target/surefire-reports/*.xml, **/target/failsafe-reports/*.xml') {
+        retainLongStdout()
+        testDataPublishers {
+            publishTestStabilityData()
         }
       }
     }
   }
-  triggers {
-    githubPush()
-  }
-  steps {
-    dsl {
-      external('jenkins/jobs/cosmic_jobs.groovy')
-    }
-  }
-}
 
-freeStyleJob(UPDATE_COSMIC_REPO_JOB) {
-  logRotator {
-    numToKeep(100)
-    artifactNumToKeep(10)
-  }
-  label('executor')
-  wrappers {
-    colorizeOutput('xterm')
-    timestamps()
-  }
-  scm {
-    git {
-      remote {
-        github(COSMIC_GITHUB_REPOSITORY, 'ssh' )
-        credentials('f4ec9d6e-49fb-497c-bd1f-e42d88e105da')
+  multiJob(fullBuildJobName) {
+    parameters {
+      stringParam(DEFAULT_GIT_REPO_BRANCH_PARAM, 'master', 'Branch to be built')
+    }
+    concurrentBuild()
+    label(executorLabel)
+    throttleConcurrentBuilds {
+      maxPerNode(1)
+    }
+    logRotator {
+      numToKeep(5)
+      artifactNumToKeep(5)
+    }
+    wrappers {
+      colorizeOutput('xterm')
+      timestamps()
+    }
+    scm {
+      git {
+        remote {
+          github(COSMIC_GITHUB_REPOSITORY, 'ssh')
+          credentials(MCCD_JENKINS_GITHUB_CREDENTIALS)
+          name('origin')
+          refspec('+refs/pull/*:refs/remotes/origin/pr/* +refs/heads/*:refs/remotes/origin/*')
+        }
+        branch('${' + DEFAULT_GIT_REPO_BRANCH_PARAM + '}')
+        clean(true)
+        recursiveSubmodules(true)
+        trackingSubmodules(true)
       }
-      branch(DEFAULT_GITHUB_REPOSITORY_BRANCH)
-      shallowClone(false)
-      clean(true)
-      recursiveSubmodules(true)
-      trackingSubmodules(true)
     }
-  }
-  triggers {
-    cron('H/15 * * * *')
-  }
-  steps {
-    shell([
-      'git config --global user.email "int-mccd_jenkins@schubergphilis.com"',
-      'git config --global user.name "mccd-jenkins"',
-      'if [ -z "$(git status -su)" ]; then',
-      '  echo "==> No submodule changed"',
-      'else',
-      '  echo "==> Updating all submodules in remote repository"',
-      '  git add --all',
-      '  git commit -m "Update all submodules to latest HEAD"',
-      '  git push origin HEAD:master',
-      'fi'
-    ].join('\n'))
-  }
-}
-
-mavenJob(GENERIC_MAVEN_JOB) {
-  parameters {
-    stringParam(GITHUB_REPOSITORY_NAME_PARAM, '', 'The GitHub repository to build')
-    stringParam('sha1', DEFAULT_GIT_REPO_BRANCH, 'Branch to be checked out and built')
-    credentialsParam(GITHUB_OAUTH2_CREDENTIAL_PARAM) {
-      type('org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl')
-      required()
-      defaultValue('95c201f6-794e-434b-a667-cf079aac4dfc')
-      description('mccd jenkins OAuth2 token credential')
-    }
-  }
-  environmentVariables {
-    env(GITHUB_OAUTH2_CREDENTIAL_PARAM, "${GITHUB_REPOSITORY_NAME_PARAM}")
-  }
-  logRotator {
-    numToKeep(50)
-    artifactNumToKeep(50)
-  }
-  concurrentBuild()
-  label('executor')
-  wrappers {
-    colorizeOutput('xterm')
-    timestamps()
-  }
-  scm {
-    git {
-      remote {
-        url('git@github.com:${' + GITHUB_REPOSITORY_NAME_PARAM + '}')
-        name('origin')
-        refspec('+refs/pull/*:refs/remotes/origin/pr/* +refs/heads/*:refs/remotes/origin/*')
-      }
-      branch('${sha1}')
-      shallowClone(true)
-      clean(true)
-    }
-  }
-  goals('clean')
-  goals('install')
-  goals('-P developer')
-  publishers {
-    archiveJunit('**/target/(surefire|failsafe)-reports/*.xml') {
-      retainLongStdout()
-      testDataPublishers {
-          publishTestStabilityData()
+    steps {
+      phase('Checkout Code, Build and Package') {
+        phaseJob(genericMavenJob) {
+          currentJobParameters(true)
+          parameters {
+            predefinedProp(CUSTOM_WORKSPACE_PARAM, WORKSPACE_VAR)
+            predefinedProp(GITHUB_REPOSITORY_NAME_PARAM, COSMIC_GITHUB_REPOSITORY)
+            predefinedProp(DEFAULT_GIT_REPO_BRANCH_PARAM, DEFAULT_GITHUB_REPOSITORY_BRANCH)
+            sameNode()
+            gitRevision(false)
+          }
+        }
       }
     }
   }

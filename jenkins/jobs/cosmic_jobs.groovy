e82d89e76fa3fd67a1d13f67cced3b31cd978005
Miguel Ferreira <miguelferreira@me.com>
2016-06-29 11:12:33 +0200

    Inject branch name into PR build

--- jenkins/jobs/cosmic_jobs.groovy
+++ jenkins/jobs/cosmic_jobs.groovy
@@ -331,6 +331,7 @@ FOLDERS.each { folderName ->
           parameters {
             sameNode()
             predefinedProp(CUSTOM_WORKSPACE_PARAM, WORKSPACE_VAR)
+            predefinedProp(GIT_REPO_BRANCH_PARAM, injectJobVariable(GIT_REPO_BRANCH_PARAM))
             predefinedProp(TESTS_PARAM, injectJobVariable(TESTS_PARAM))
             gitRevision(true)
           }
